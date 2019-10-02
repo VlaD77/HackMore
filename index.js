c2d631091dff858b2a67ca452db52b4358f702a7
vlad kuradovets <vlad.kuradovets@meliorgames.com>
2019-10-02 18:08:59 +0300

    [DELTET] about route

--- index.js
+++ index.js
@@ -8,8 +8,4 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.get("/about", (req, res) => {
-  res.send("About");
-});
-
 app.listen({ port: process.env.PORT }, () => console.log("running"));
